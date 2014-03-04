3d88b41809d5abcb633a1ce1a7df4cd7a76d033b
Jeremy Crawford <jcrawford.design@gmail.com>
2014-03-04 11:35:07 -0500

    Initial Commit

---
+++ app/scripts/main-view.js
@@ -0,0 +1,24 @@
+// Main View is responsible for instantiating
+// the TopBarView and.....?
+
+var MainView = Backbone.View.extend({
+
+    className: 'container',
+
+    initialize: function(options) {
+
+      window.user = new User();
+      user.fetch();
+
+      setInterval(function() {
+        user.fetch();
+      }, 5000);
+
+      $('body').prepend(this.el)
+      this.render();
+    },
+
+    render: function() {
+      new TopBarView({model: user});
+    }
+});
---
+++ app/scripts/user-model.js
@@ -0,0 +1,3 @@
+var User = Backbone.Model.extend({
+  url: "http://tiny-pizza-server.herokuapp.com/collections/users/5315fe6a589c150200000006",
+});
